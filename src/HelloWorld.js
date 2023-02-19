5672026b5d8b7f80091a484cdf7e32a43db3b7cf
TeddyCatTX <kay.dmz@gmail.com>
2023-02-19 13:33:19 -0600

    add a link

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './HelloWorld.css';
- 
+
 class HelloWorld extends Component {
+  handleClick = () => {
+    window.open('https://www.kirupa.com/react/setting_up_react_environment.htm', '_blank');
+  };
+
   render() {
     return (
       <div className="helloContainer">
         <h1>Hello, world!</h1>
+        <br />
+        <button onClick={this.handleClick}>Creating our HelloWorld App</button>
       </div>
     );
   }
 }
- 
+
 export default HelloWorld;
