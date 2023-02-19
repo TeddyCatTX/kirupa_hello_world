96bd548acc8ce0f16a0ab34db9617beb360a87f8
TeddyCatTX <kay.dmz@gmail.com>
2023-02-19 16:17:37 -0600

    Testing

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -9,10 +9,10 @@ class HelloWorld extends Component {
   render() {
     return (
       <div className="helloContainer">
-        <h1>Hello, world!</h1>
+        <h1>Hello, world! <a href="https://www.google.com" target="_blank">Go to Google</a></h1>
         <br />
         <button onClick={this.handleClick}>Creating our HelloWorld App</button>
-        <a href="https://www.google.com" target="_blank">Go to Google</a>
+        
       </div>
     );
   }
